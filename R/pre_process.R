e3233b6042922f359533251adb76c05553930b23
izar-de-villasante <idevillasante@carrerasresearch.org>
2022-06-21 12:15:16 +0200

    fixed pre-process read.metharray

--- R/pre_process.R
+++ R/pre_process.R
@@ -13,6 +13,10 @@
 #' @param RGset Whether you want normalised "RGChannelSet" or
 #' "RGChannelSetExtended" to be saved or not. path=out
 #' @param arraytype Methylation array type
+#' @param idats_folder Folder containing idats. It will try to guess which idat
+#' belongs to which row in the targets data.frame.
+#' @param copy Change to TRUE if you want to copy files to idats folder.
+#' Useful when working in HPC and idats are in ISILON.
 #' @inheritParams run_cnv.methyl
 #' @return Log2r intensities ready to be analysed with conumee.
 #' @export
@@ -23,14 +27,66 @@
 #' ss<-TrainingSet_Sample_sheet[1:56,]
 #' pre_process(ss)
 
-pre_process<-function(targets,purity=NULL,query=T,RGset=T,out="analysis/intermediate/",subf="IDATS/",folder=NULL,ncores=NULL,arraytype="450K"){
+pre_process<-function(targets,purity=NULL,query=T,RGset=T,out="./analysis/intermediate/",idats_folder=NULL,subf="IDATS/",folder=NULL,ncores=NULL,arraytype="450K",copy=FALSE){
   #data('hm450.manifest.hg19')
   if (!is.character(folder)) folder<-paste0(out,subf)
   dir.create(folder,recursive=TRUE)
-  targets$Basename<-paste0(folder, basename(targets$filenames))
-  myLoad<-pre_process.myLoad(targets,folder=folder, arraytype=arraytype,nocres=ncores)
+  if (!is.null(targets)) {
+    if (!"Basename" %in% names(targets)) {
+      warning("Need 'Basename' amongst the column names of 'targets', will construct from idats.folder")
+      if(is.null(idats_folder)){
+        stop("Either provide Basename or a valid idats.foler in order to continue" )
+      }else{
+        base<-idats_folder
+        Grn.files <- list.files(path = base, pattern = "_Grn.idat$",
+                                recursive = T, ignore.case = TRUE, full.names = TRUE)
+        Red.files <- list.files(path = base, pattern = "_Red.idat$",
+                                recursive = T, ignore.case = TRUE, full.names = TRUE)
+        if (length(Grn.files) == 0 || length(Red.files) == 0) {
+          stop("No IDAT files were found")
+        }
+        commonFiles <- intersect(x = sub("_Grn.idat$", "", Grn.files),
+                                 y = sub("_Red.idat$", "", Red.files))
+        if (length(commonFiles) == 0) {
+          stop("No IDAT files with both Red and Green channel were found")
+        }
+        commonFiles.Grn <- paste(commonFiles, "_Grn.idat", sep = "")
+        commonFiles.Red <- paste(commonFiles, "_Red.idat", sep = "")
+        if (!setequal(commonFiles.Grn, Grn.files)) {
+          warning(sprintf("the following files only exists for the green channel: %s",
+                          paste(setdiff(Grn.files, commonFiles.Grn), collapse = ", ")))
+        }
+        if (!setequal(commonFiles.Red, Red.files)) {
+          warning(sprintf("the following files only exists for the red channel: %s",
+                          paste(setdiff(Red.files, commonFiles.Red), collapse = ", ")))
+        }
+
+        # Try to figure out which file belongs to which sample
+        best<-NULL
+        nbest<- round(dim(targets)[1]*0.9)
+        counts<-sapply(names(targets),function(column){
+          sum(sapply(basename(commonFiles),function(s)  targets[,..column] %like% s))
+        })
+        if(max(counts)>nbest){
+          best<- which.max(counts)
+        }else{
+          warning(paste0("only ",counts, "out of ", nrow(targets), "samples in sample sheet matched" ))
+          best<- which.max(counts)
+        }
+        targets[sapply(basename(commonFiles),function(sample){which(sapply(targets[,..best],function(x) x %like% sample))}),Basename:=commonFiles]
+
+      }
+    }
+    files<-targets$Basename
+
+    }else{
+    stop("Must provide a 'targets' data.frame in order to continue")
+  }
+
+
+  myLoad<-pre_process.myLoad(targets,folder=folder, files=files, arraytype=arraytype,copy=copy, ncores=ncores)
   saveRDS(myLoad,paste0(out,"myLoad.rds"))
-  targets<-SummarizedExperiment::colData(myLoad)
+  #targets<-SummarizedExperiment::colData(myLoad)
   if(is.null(purity)){
     purity<-purify(myLoad=myLoad)
     message("Rfpurifies")
@@ -50,47 +106,12 @@ pre_process<-function(targets,purity=NULL,query=T,RGset=T,out="analysis/intermed
 
 }
 
-#' DEPRECATED!
-#' prepare idats folder and targets csv in parallel using foreach
-#' Now part of read.metharray.exp.par
-#'
-#' @title generate champ folder with all idats
-#' @param targets data.frame representing valid targets file as is
-#' @param ncores cores
-#' @param folder workdir where to copy files
-#' used within the minfi package
-#' @return folder with idats
-#' @author izar de Villasante
-#' @export
-#' @importFrom fs file_copy
-#' @importFrom foreach foreach
-#' @importFrom foreach %dopar%
-idats_folder <- function(targets,folder,ncores=NULL){
-  ncores<-get_ncores(ncores)
-  cl<- parallel::makePSOCKcluster(ncores)
-  doParallel::registerDoParallel(cl)
-  res<-foreach::foreach(it=itertools::isplitIndices(nrow(targets), chunks=ncores),#isplitIndices(1400,chunks=ncores),
-                        .combine='c',
-                        .multicombine = F,
-                        .inorder=F,
-                        .errorhandling = "pass"
-  )%dopar%{
-    requireNamespace("fs")
-    subdf<-as.data.frame(targets[it,])
-    fs::file_copy(paste0(subdf$filenames,"_Grn.idat"),new_path=folder,overwrite = T)
-    fs::file_copy(paste0(subdf$filenames,"_Red.idat"),new_path=folder,overwrite=T)
-
-  }
-  parallel::stopCluster(cl)
-}
-
 #' construct RGChannelSet in parallel using foreach
 #'
 #'
 #' @title construct RGChannelSet in parallel
 #'
-#' @param verbose logical default TRUE
-#' @param ... optional arguments to read.metharray.exp
+
 #' @inheritParams run_cnv.methyl
 #' @return RGset
 #' @author izar de Villasante
@@ -99,34 +120,48 @@ idats_folder <- function(targets,folder,ncores=NULL){
 #' @importFrom utils str
 #' @importFrom BiocGenerics cbind combine
 #' @importFrom foreach foreach
-#' @importFrom foreach %dopar%
+#' @importFrom minfi read.metharray
+#' @inheritParams minfi::read.metharray
 
 
 
 
-read.metharray.exp.par <- function(targets,folder, verbose = TRUE,arraytype="450K",ncores=NULL, ...) {
+
+
+read.metharray.exp.par <- function(targets,folder,files,copy=FALSE, verbose = TRUE, arraytype="450K",ncores=NULL, extended = FALSE, force =FALSE){
   #Make cluster:
   ncores<-get_ncores(ncores)
   message("Reading multiple idat-files in parallel. Using ",ncores," cores.")
   cl<- parallel::makePSOCKcluster(ncores)
   doParallel::registerDoParallel(cl)
+  if (copy ==TRUE){
+    files<-paste0(folder,basename(targets$Basename))
+  }
+  requireNamespace("S4Vectors")
   res<-foreach::foreach(it=itertools::isplitIndices(nrow(targets), chunks=ncores),#isplitIndices(1400,chunks=ncores),
-               .combine='cbind',
-               .multicombine = F,
-               .inorder=F,
-               .export= ...,
-               .errorhandling = "pass"
+                        .combine='cbind',
+                        .multicombine = F,
+                        .inorder=F,
+                        .errorhandling = "pass"
   )%dopar%{
-    requireNamespace("minfi")
-    requireNamespace("fs")
+    requireNamespace(c("minfi","fs"))
     subdf<-as.data.frame(targets[it,])
-    fs::file_copy(paste0(subdf$filenames,"_Grn.idat"),new_path=folder,overwrite = T)
-    fs::file_copy(paste0(subdf$filenames,"_Red.idat"),new_path=folder,overwrite=T)
-    rgSet<-minfi::read.metharray.exp(targets = subdf, base=folder, ...)
+    if (copy ==TRUE){
+      fs::file_copy(paste0(subdf$filenames,"_Grn.idat"),new_path=folder,overwrite = T)
+      fs::file_copy(paste0(subdf$filenames,"_Red.idat"),new_path=folder,overwrite=T)
+
+    }
+
+    rgSet<-minfi::read.metharray(basenames = files[it], extended = extended, verbose = verbose, force =force)
     if(arraytype=="EPIC") rgSet@annotation <- c(array="IlluminaHumanMethylationEPIC",annotation="ilm10b4.hg19")
     return(rgSet)
   }
   parallel::stopCluster(cl)
+  pD <- data.frame(targets)
+  pD$filenames <- files
+  rownames(pD) <- colnames(res)
+  res@colData <- methods::as(pD, "DataFrame")
+  #rownames(res@colData)<-colnames(res)
   return(res)
 }
 
@@ -140,12 +175,12 @@ read.metharray.exp.par <- function(targets,folder, verbose = TRUE,arraytype="450
 #' @return RGset
 #' @author izar de Villasante
 #' @export
-pre_process.myLoad <-function(targets,folder,arraytype="450K",ncores=NULL, ...) {
+pre_process.myLoad <-function(targets,folder,arraytype="450K",ncores=NULL, files=NULL, copy=FALSE) {
   message("working directory: ",folder)
   file.remove(list.files(folder, full.names = TRUE))
   #idats_folder(targets,folder = folder)
   utils::write.csv(targets,paste0(folder,"/Sample_sheet.csv"))
-  myLoad <- read.metharray.exp.par(targets=targets,folder = folder,arraytype=arraytype,ncores=ncores)
+  myLoad <- read.metharray.exp.par(targets=targets,files=files,folder = folder,arraytype=arraytype,ncores=ncores,copy=copy)
   message("Minfi does load data")
   return(myLoad)
 }
@@ -203,5 +238,4 @@ queryfy<-function(myLoad){
   # mSetSqn <- CNV.load(mSetSqn)
   # return(mSetSqn@intensity)
 }
-
 
